a5de57dfff99f9c9c8a3b3d5feaf761f8af2e5d3
alex.fan <alex.fan@okchem.com>
2017-10-05 22:31:20 +0800

    second commit with transform into a Vue component style

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
-const app = new Vue({
-    el: "#app",
-    data: {
-        friends: [
-            {
-                first: 'Alex',
-                last: 'Fan',
-                age: 37
-            },
-            {
-                first: 'Sky',
-                last: 'Fan',
-                age: 7
-            }
-        ],
+Vue.component('friend-component', {
+  props: ['friend'],
+  filters: {
+    fullName(value) {
+      return `${value.first} ${value.last}`
+    }
+  },
+  methods: {
+    incrementAge(friend){
+      friend.age = friend.age + 1
     },
-    filters: {
-        fullName(value) {
-            return `${value.first} ${value.last}`
-        }
+    decrementAge(friend){
+      friend.age = friend.age - 1
     },
-    methods: {
-        incrementAge(friend){
-            friend.age = friend.age + 1
-        },
-        decrementAge(friend){
-            friend.age = friend.age - 1
-        },
-    },
-    template: `
+  },
+  template: `
     <div>
-      <div v-for="friend in friends">
-         <h2>{{friend | fullName}}</h2>
-         <input v-model="friend.first"/>
-         <h2>
-             <button v-on:click="decrementAge(friend)">-</button>
-             Age: {{friend.age}}
-             <button v-on:click="incrementAge(friend)">+</button>
-         </h2>
-      </div>
+      <h2>{{friend | fullName}}</h2>
+        <input v-model="friend.first"/>
+      <h2>
+        <button v-on:click="decrementAge(friend)">-</button>
+        Age: {{friend.age}}
+        <button v-on:click="incrementAge(friend)">+</button>
+      </h2>
+    </div>
+  `
+})
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    friends: [
+      {
+        first: 'Alex',
+        last: 'Fan',
+        age: 37
+      },
+      {
+        first: 'Sky',
+        last: 'Fan',
+        age: 7
+      }
+    ],
+  },
+  template: `
+    <div>
+      <friend-component v-for="item in friends" v-bind:friend="item"/>
     </div>
   `
 })
